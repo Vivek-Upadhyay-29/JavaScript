5d1fee063914b398e9ab4292f278c636ea4eff6a
Vivek Upadhyay <128276747+Vivek-Upadhyay-29@users.noreply.github.com>
2024-03-31 09:05:19 +0530

    Add files via upload

---
+++ JS/practice.js
@@ -0,0 +1,90 @@
+// var guestName = prompt("enter your name");
+// var guestList = ["Chinu","chandu","Parshu"] ;
+// if(guestList.includes(guestName)){
+//     console.log("Welcome buddy!!");
+// }
+// else{
+// console.log("Sorry not welcomed");
+// }
+// let a = null;
+// let b =  "string";
+// let c = BigInt("65165");
+// let d = true; 
+// let e = Symbol("This is a symbol");
+// let f = undefined; 
+
+// console.log(typeof(b))
+// const dict = {
+//     vivek : "shanti",
+//     chandu : " gandu",
+//     parshu : "chaudibaz"
+// }
+// console.log(dict.chandu)
+// let n = prompt("enter a number");
+// n = parseInt(n);
+
+// let i = 0;
+// // while(n>99){
+// //     console.log("greater");
+// // }
+// do{
+//     console.log(i);
+//     i++;
+// } while(i < n )
+
+// let n = prompt("Enter your age");
+// n = parseInt(n);
+// function checker(age) 
+// {
+//     if (age < 18){
+//         console.log("Chota baccha") ;
+//     }
+//     else 
+//     {
+//         console.log("Bada baccha");
+//     }
+// }
+
+// checker(n);
+
+// let marks = {
+//     Vivek: "100",
+//     Chandu : "80",
+//     Parshu : "30"
+// }
+// let i = 0;
+// console.log(marks.Parshu);
+// for(i=0 ;i<Object.keys(marks).length ; i++){
+//     console.log("The marks of " + Object.keys(marks)[i] +" is " + marks[Object.keys(marks)[i]])
+
+// }
+// let x = "763487642336462384";
+// let y = parseInt(x);
+// let name = "Vivek";
+// let surname = "Upadhyay";
+// for(let i = 0; i<name.length;i++){
+//     console.log(name[i])
+// }
+// console.log(name.toUpperCase())
+// console.log(name.toLowerCase())
+// console.log(name.slice(1,2))
+// console.log(name.replace("Vivek","Vigek"))
+// console.log(name.concat("Is my Sirname"+surname))
+// console.log(typeof(y))
+// let marks_students = [29,35,65,65,"vivek",null]
+// marks_students.splice(2,3,45,54,44)
+
+// let marks_students3 = [29,35,79,65,"rakesh",null]
+// // for(let i = 0; i <marks_students.length; i++){
+// // console.log(marks_students[i]) 
+// // }
+
+// console.log(marks_students)
+
+// console.log(marks_students.concat(marks_students2,marks_students3))
+let marks_students2 = "vivek";
+let name = Array.from(marks_students2)
+console.log(typeof name)
+for(let name of i){
+    console.log(i)
+}
